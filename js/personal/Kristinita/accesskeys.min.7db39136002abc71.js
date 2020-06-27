ec7c36bcf4fb5574b6e8d56564586628f40e3265
Саша Черных (via Travis CI) <vspobog@gmail.com>
2020-06-27 15:43:28 +0000

    Commit https://github.com/Kristinita/KristinitaPelican/commit/afb85d58d82cc97379fb800b3a6ad72b2a42e386 successfully builded and checked by Travis CI build 101. See https://travis-ci.org/Kristinita/KristinitaPelican/builds/702681551 for details.

--- js/personal/Kristinita/accesskeys.min.7db39136002abc71.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){var t;t=function(){0===$("#gsc-i-id1").length?setTimeout(t,500):$("#gsc-i-id1").attr("accesskey","k")},setTimeout(t,500)});
-//# sourceMappingURL=accesskeys.min.js.map
